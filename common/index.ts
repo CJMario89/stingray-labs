b81921295f75d7cb1f4e8cbeeedee51d20909b72
cjmario <mario89cheng@gmail.com>
2025-01-05 13:45:40 +0800

    fixup! chore: remove unused code

--- common/index.ts
+++ common/index.ts
@@ -147,6 +147,7 @@ export async function fetchWithRetry(
 }
 
 export function getCookie(name: string) {
+  if (typeof document === "undefined") return;
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop()?.split(";").shift();
